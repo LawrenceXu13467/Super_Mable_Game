b5580c8e55480d9cf52be063604b6910cdd5b969
Lawrence <x15026471536@icloud.com>
2018-01-05 13:38:25 +0800

    folder added

---
+++ Super Marble Project/Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform ball; // Transform is position info
+	private Vector3 offset; // 3d vector named offset
+
+	// Use this for initialization
+	void Start () {
+		offset = this.transform.position - ball.position; // a vector pointing from the ball to the camera
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = ball.position + offset;
+
+	}
+}
---
+++ Super Marble Project/Assets/TeleportZone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportZone : MonoBehaviour {
+
+	public Transform spawnpoint;
+
+	void OnTriggerEnter(Collider other){
+		other.transform.position = spawnpoint.position;
+		Rigidbody rb = other.GetComponent<Rigidbody>();
+		rb.Sleep();
+
+	}
+
+
+	// Not gonna use the code below
+//	// Use this for initialization
+//	void Start () {
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+}
